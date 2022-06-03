b1730e88373a5aa6396b23306bbf9e0f0bf20431
Ritz_ <ritz1229@icloud.com>
2022-06-03 17:19:47 +0900

    add: dijkstraTest.kt

---
+++ src/test/kotlin/graph/dijkstra/DijkstraTest.kt
@@ -0,0 +1,20 @@
+package graph.dijkstra
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class DijkstraTest() {
+    @Test
+    internal fun dijkstra_solveNormalCondition() {
+        val ans = Dijkstra.solve()
+        val expectedAnswer = listOf<List<Int>>(
+            listOf(0),
+            listOf(0, 1),
+            listOf(0, 5, 4, 3, 2),
+            listOf(0, 5, 4, 3),
+            listOf(0, 5, 4),
+            listOf(0, 5)
+        )
+        assertEquals(expectedAnswer, ans)
+    }
+}
