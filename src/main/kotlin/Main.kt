58fa621e59d677221eff4c9c6f04ea7130d3a600
Ritz_ <ritz1229@icloud.com>
2022-06-03 09:29:25 +0900

    first commit：ダイクストラ法の実装をするレポジトリ

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+//package graph
+
+import graph.dijkstra.Dijkstra
+
+// private const val MAX_COST = Int.MAX_VALUE
+
+fun main(args: Array<String>) {
+    val ans = Dijkstra.solve()
+    println(ans)
+}
+
+// private fun readInts(n: Int) = readLine()!!.split(" ").map(String::toInt).slice(0 until n)
