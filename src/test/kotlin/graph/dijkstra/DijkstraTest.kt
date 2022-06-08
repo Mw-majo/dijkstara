07f213be7aa0524f7009c9a7f6272ce46ecd9f20
Ritz_ <ritz1229@icloud.com>
2022-06-08 09:22:24 +0900

    add: 入出力を受け取るためにInputStream, OutputStreamを導入 入力から問題を解けるようになった

--- src/test/kotlin/graph/dijkstra/DijkstraTest.kt
+++ src/test/kotlin/graph/dijkstra/DijkstraTest.kt
@@ -1,23 +1,65 @@
 package graph.dijkstra
 
+import io.StandardInputStream
+import io.StandardOutputStream
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 internal class DijkstraTest() {
-    val edges = listOf(
-        listOf(0, 1, 6),
-        listOf(0, 5, 3),
-        listOf(1, 2, 3),
-        listOf(2, 3, 1),
-        listOf(2, 4, 3),
-        listOf(2, 5, 5),
-        listOf(3, 4, 1),
-        listOf(4, 5, 2)
-    )
+
+    private val input = StandardInputStream()
+    private val output = StandardOutputStream()
+
+
+    @BeforeEach
+    fun setUp() {
+        System.setIn(input)
+        System.setOut(output)
+    }
+
+    @AfterEach
+    fun tearDown(){
+        System.setIn(null)
+        System.setOut(null)
+    }
+
+//    val edges = listOf(
+//        listOf(0, 1, 6),
+//        listOf(0, 5, 3),
+//        listOf(1, 2, 3),
+//        listOf(2, 3, 1),
+//        listOf(2, 4, 3),
+//        listOf(2, 5, 5),
+//        listOf(3, 4, 1),
+//        listOf(4, 5, 2)
+//    )
 
     @Test
     internal fun dijkstra_solveNormalCondition() {
-        val ans = Dijkstra(6, edges).ansList[0]
+        input.inputln("6 8")
+        input.inputln("0 1 6")
+        input.inputln("0 5 3")
+        input.inputln("1 2 3")
+        input.inputln("2 3 1")
+        input.inputln("2 4 3")
+        input.inputln("2 5 5")
+        input.inputln("3 4 1")
+        input.inputln("4 5 2")
+
+        val (nodeNumber, edgeNumber) = readLine()!!
+            .split(" ")
+            .slice(0..1)
+            .map { it.toInt() }
+        val edges = List(edgeNumber) {
+            readLine()!!
+                .split(" ")
+                .slice(0..2)
+                .map{ it.toInt() }
+        }
+
+        val ans = Dijkstra(nodeNumber, edges).ansList[0]
         val expectedAnswer = listOf<List<Int>>(
             listOf(0),
             listOf(0, 1),
